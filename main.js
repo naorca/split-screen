2a77ede56d71bae8e124df60d3bf42c615b8a356
Naor Cohen Adiv <naorca@gmail.com>
2019-02-11 12:59:10 +0200

    Fixed js

---
+++ main.js
@@ -0,0 +1,50 @@
+// document.addEventListener('DOMContentLoaded', function(){
+//   let wrapper = document.getElementById('wrapper');
+//   let topLayer = wrapper.querySelector('.top');
+//   let handle = wrapper.querySelector('.handle');
+//   let skew = 0;
+//   let delta =0;
+
+
+//   if(wrapper.className.indexOf('skewed') != -1){
+//     skew = 1010;
+  
+//   }
+  
+//   wrapper.addEventListener('mousemove', function(e){
+//     handle.style.left = e.clientX + delta + 'px';
+//     topLayer.style.width = e.clientX + skew + 'px';
+//   })
+
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+  let wrapper = document.getElementById('wrapper');
+  let topLayer = wrapper.querySelector('.top');
+  let handle = wrapper.querySelector('.handle');
+  let skew = 0;
+  let delta = 0;
+
+  if (wrapper.className.indexOf('skewed') != -1) {
+    skew = 1000;
+  }
+
+  function positionHandler(e) {
+    if ((e.clientX) && (e.clientY)) {
+      delta = (e.clientX - window.innerWidth / 2) * 0.5;
+      handle.style.left = e.clientX + delta + 'px';
+      topLayer.style.width = e.clientX + skew + delta + 'px';
+
+    } else if (e.targetTouches) {
+      delta = (e.targetTouches[0].clientX - window.innerWidth / 2) * 0.5;
+      handle.style.left = e.targetTouches[0].clientX + delta + 'px';
+      topLayer.style.width = e.targetTouches[0].clientX + skew + delta + 'px';
+      e.preventDefault();
+    }
+  };
+
+  wrapper.addEventListener('mousemove', positionHandler, false);
+  wrapper.addEventListener('touchstart', positionHandler, false);
+  wrapper.addEventListener('touchmove', positionHandler, false);
+
+});
